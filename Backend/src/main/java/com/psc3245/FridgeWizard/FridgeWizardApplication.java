c2dc0edf80f727a20d63624a37427845d5ee4c60
psc3245 <psc3.omaha@gmail.com>
2024-11-26 20:17:44 -0600

    Backend starter

---
+++ Backend/src/main/java/com/psc3245/FridgeWizard/FridgeWizardApplication.java
@@ -0,0 +1,13 @@
+package com.psc3245.FridgeWizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeWizardApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/psc3245/FridgeWizard/FridgeWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc3245.FridgeWizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
